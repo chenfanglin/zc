b9f6a5806fc5107ecfa9e35b1bd6e03f49dae71d
sunny <sunnychen@aijiatui.com>
2018-06-02 20:12:27 +0800

    sql

--- zc/createDb.sql
+++ zc/createDb.sql
@@ -17,7 +17,22 @@ create table t_patent(
 	`is_batch` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否批量:0不批量,1批量',
 	`publish_year` int(11) NOT NULL DEFAULT '0' COMMENT '发布年份',
 	`publish_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+	`user_qq` int(11) NOT NULL DEFAULT '0' COMMENT '用户qq',
+	`user_wx` varchar(30) NOT NULL DEFAULT '' COMMENT '用户wx',
+	`show` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否显示:0显示,1不显示',
 	PRIMARY KEY (`id`),
 	INDEX `patentId` (`patent_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='专利表';
 
+create table IF NOT EXISTS t_users; 
+
+create table t_users(
+	`id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键,和业务无关,提高存取效率',
+	`user_name` varchar(30) NOT NULL  DEFAULT '' COMMENT '用户名',
+	`user_qq` int(11) NOT NULL DEFAULT '0' COMMENT '用户qq',
+	`user_wx` varchar(30) NOT NULL DEFAULT '' COMMENT '用户wx',
+	`user_phone` varchar(30) NOT NULL DEFAULT '' COMMENT '用户手机号',
+	`user_email` varchar(30) NOT NULL DEFAULT '' COMMENT '用户邮箱',
+	`password` varchar(30) NOT NULL DEFAULT '' COMMENT '用户密码',
+	PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
